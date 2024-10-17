58e6f4cd0670eebeecd449c56022e674f7608983
Omar Khattab <omar.khattab7@icloud.com>
2024-10-17 13:02:11 +0300

    Add basic login form

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Inventory_Managment_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory_Managment_System.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Implement your authentication logic here
+                // For example, check the user's credentials against your database
+                // If valid, sign in the user and redirect to a secured page
+                // If invalid, add a ModelState error and return the view
+            }
+            return View(model);
+        }
+    }
+}
