e634af3d002bb04ec03bbea050dd67fd0244e37a
Mohamed Hussein <muhammed.tammaa@gmail.com>
2024-10-21 00:57:50 +0300

    migration

---
+++ Migrations/20241020215001_fk_for_orderDetails.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory_Managment_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class fk_for_orderDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_orderDetails_orderId",
+                table: "orderDetails",
+                column: "orderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orderDetails_orders_orderId",
+                table: "orderDetails",
+                column: "orderId",
+                principalTable: "orders",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orderDetails_orders_orderId",
+                table: "orderDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_orderDetails_orderId",
+                table: "orderDetails");
+        }
+    }
+}
---
+++ Migrations/20241020215313_lastmigration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory_Managment_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class lastmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_orderDetails_productId",
+                table: "orderDetails",
+                column: "productId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_orderDetails_products_productId",
+                table: "orderDetails",
+                column: "productId",
+                principalTable: "products",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_orderDetails_products_productId",
+                table: "orderDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_orderDetails_productId",
+                table: "orderDetails");
+        }
+    }
+}
