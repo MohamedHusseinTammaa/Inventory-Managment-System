ddd69fd70c0224e0d9ccde3283260d42a974afab
Omar Khattab <omar.khattab7@icloud.com>
2024-10-21 09:02:27 +0300

    Update some UI

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Inventory_Managment_System.Interfaces;
+﻿using Inventory_Managment_System.Data;
+using Inventory_Managment_System.Interfaces;
+using Inventory_Managment_System.Models.Classes;
 using Inventory_Managment_System.Models.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,18 +9,15 @@ namespace Inventory_Managment_System.Controllers
     public class ProductController : Controller
     {
         public readonly IProduct product; 
+        public readonly InventoryDbContext _context;
         public ProductController (IProduct productServices)
         {
             product = productServices;
         }
         public IActionResult Index()
         {
-            return View();
-        }
-        public IActionResult getAllProduts() 
-        { 
-            var list = product.getAllProducts();
-            return View(list);
+            List<Product> products = _context.products.ToList();
+            return View("getAllProducts", products);
         }
     }
 }
