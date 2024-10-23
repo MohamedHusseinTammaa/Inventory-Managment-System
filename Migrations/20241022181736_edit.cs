89e2b7294ed1f750c009ecab2711751a30b37bdd
Mohamed Hussein <muhammed.tammaa@gmail.com>
2024-10-23 10:57:51 +0300

    serrrr

---
+++ Migrations/20241022181736_edit.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory_Managment_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class edit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseDetails_purchases_Purchaseid",
+                table: "PurchaseDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PurchaseDetails_Purchaseid",
+                table: "PurchaseDetails");
+
+            migrationBuilder.DropColumn(
+                name: "Purchaseid",
+                table: "PurchaseDetails");
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastUserId",
+                table: "brands",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "createdUserId",
+                table: "brands",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastUserId",
+                table: "brands");
+
+            migrationBuilder.DropColumn(
+                name: "createdUserId",
+                table: "brands");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Purchaseid",
+                table: "PurchaseDetails",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PurchaseDetails_Purchaseid",
+                table: "PurchaseDetails",
+                column: "Purchaseid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseDetails_purchases_Purchaseid",
+                table: "PurchaseDetails",
+                column: "Purchaseid",
+                principalTable: "purchases",
+                principalColumn: "id");
+        }
+    }
+}
---
+++ Services/SupplierServices.cs
@@ -0,0 +1,23 @@
+﻿using Inventory_Managment_System.Interfaces;
+using Inventory_Managment_System.Models.Classes;
+using System;
+using System.Collections.Generic;
+using Inventory_Managment_System.Data;
+
+namespace Inventory_Managment_System.Services
+{
+    public class SupplierServices : ISupplier
+    {
+        private readonly InventoryDbContext _context;
+
+        public SupplierServices(InventoryDbContext context)
+        {
+            _context = context;
+        }
+        public List<Supplier> getAllSuppliers()
+        {
+            var list = _context.suppliers.ToList();
+            return list;
+        }
+    }
+}
