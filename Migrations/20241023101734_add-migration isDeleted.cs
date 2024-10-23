0d38ec844705beb0b0a8eae619ff8ba102199492
Mohamed Hussein <muhammed.tammaa@gmail.com>
2024-10-23 13:18:51 +0300

    isDeleted

---
+++ Migrations/20241023101734_add-migration isDeleted.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory_Managment_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmigrationisDeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "products");
+        }
+    }
+}
