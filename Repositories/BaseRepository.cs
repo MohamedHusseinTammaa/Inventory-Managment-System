5d1ad3b88c35f9a029b4125a15227d04cbf0b376
Omar Khattab <omar.khattab7@icloud.com>
2025-03-23 14:56:58 +0200

    Add XML Documentation to Repository & UnitOfWork Classes

--- Repositories/BaseRepository.cs
+++ Repositories/BaseRepository.cs
@@ -4,27 +4,51 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Inventory_Managment_System.Repositories
 {
+    /// <summary>
+    /// Generic repository for performing CRUD operations on entities.
+    /// </summary>
+    /// <typeparam name="T">The type of the entity.</typeparam>
     public class BaseRepository<T> : IBaseRepository<T> where T : class
     {
         protected readonly InventoryDbContext _context;
         protected readonly DbSet<T> _dbSet;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseRepository{T}"/> class.
+        /// </summary>
+        /// <param name="context">The database context.</param>
         public BaseRepository(InventoryDbContext context)
         {
             _context = context;
             _dbSet = context.Set<T>();
         }
 
+        /// <summary>
+        /// Gets an entity by its identifier.
+        /// </summary>
+        /// <param name="id">The identifier of the entity.</param>
+        /// <returns>The entity with the specified identifier.</returns>
         public T getByID(int id)
         {
             return _dbSet.Find(id);
         }
 
+        /// <summary>
+        /// Gets all entities asynchronously.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the list of entities.</returns>
         public async Task<IEnumerable<T>> getAllAsync()
         {
             return await _dbSet.ToListAsync();
         }
 
+        /// <summary>
+        /// Finds entities that match the specified expression with optional pagination.
+        /// </summary>
+        /// <param name="expression">The expression to filter entities.</param>
+        /// <param name="take">The number of entities to take.</param>
+        /// <param name="skip">The number of entities to skip.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the list of matching entities.</returns>
         public async Task<IEnumerable<T>> findAllAsync(Expression<Func<T, bool>> expression, int? take, int? skip)
         {
             IQueryable<T> query = _dbSet.Where(expression);
@@ -38,34 +62,62 @@ namespace Inventory_Managment_System.Repositories
             return await query.ToListAsync();
         }
 
+        /// <summary>
+        /// Adds a new entity.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <returns>The added entity.</returns>
         public T add(T entity)
         {
             _dbSet.Add(entity);
             return entity;
         }
 
+        /// <summary>
+        /// Adds a new entity asynchronously.
+        /// </summary>
+        /// <param name="entity">The entity to add.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the added entity.</returns>
         public async Task<T> addAsync(T entity)
         {
             await _dbSet.AddAsync(entity);
             return entity;
         }
 
+        /// <summary>
+        /// Deletes an entity.
+        /// </summary>
+        /// <param name="entity">The entity to delete.</param>
         public void delete(T entity)
         {
             _dbSet.Remove(entity);
         }
 
+        /// <summary>
+        /// Updates an entity.
+        /// </summary>
+        /// <param name="entity">The entity to update.</param>
+        /// <returns>The updated entity.</returns>
         public T update(T entity)
         {
             _context.Entry(entity).State = EntityState.Modified;
             return entity;
         }
 
+        /// <summary>
+        /// Counts all entities asynchronously.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the count of entities.</returns>
         public async Task<int> countAllAsync()
         {
             return await _dbSet.CountAsync();
         }
 
+        /// <summary>
+        /// Counts entities that match the specified expression asynchronously.
+        /// </summary>
+        /// <param name="expression">The expression to filter entities.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the count of matching entities.</returns>
         public async Task<int> countSpecificItems(Expression<Func<T, bool>> expression)
         {
             return await _dbSet.CountAsync(expression);
--- Repositories/UnitOfWork.cs
+++ Repositories/UnitOfWork.cs
@@ -7,18 +7,30 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Inventory_Managment_System.UnitOfWork
 {
+    /// <summary>
+    /// Unit of Work class that coordinates the work of multiple repositories.
+    /// </summary>
     public class UnitOfWork : IUnitOfWork
     {
         private readonly InventoryDbContext _context;
         private readonly Dictionary<Type, object> _repositories;
         private bool _disposed;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnitOfWork"/> class.
+        /// </summary>
+        /// <param name="context">The database context.</param>
         public UnitOfWork(InventoryDbContext context)
         {
             _context = context;
             _repositories = new Dictionary<Type, object>();
         }
 
+        /// <summary>
+        /// Gets the repository for the specified entity type.
+        /// </summary>
+        /// <typeparam name="T">The type of the entity.</typeparam>
+        /// <returns>The repository for the specified entity type.</returns>
         public IBaseRepository<T> Repository<T>() where T : class
         {
             var type = typeof(T);
@@ -31,22 +43,37 @@ namespace Inventory_Managment_System.UnitOfWork
             return (IBaseRepository<T>)_repositories[type];
         }
 
+        /// <summary>
+        /// Saves all changes made in this context to the database.
+        /// </summary>
+        /// <returns>The number of state entries written to the database.</returns>
         public int Complete()
         {
             return _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Asynchronously saves all changes made in this context to the database.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous save operation. The task result contains the number of state entries written to the database.</returns>
         public async Task<int> CompleteAsync()
         {
             return await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Disposes the context.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
+        /// <summary>
+        /// Disposes the context.
+        /// </summary>
+        /// <param name="disposing">A boolean value indicating whether the context is being disposed.</param>
         protected virtual void Dispose(bool disposing)
         {
             if (!_disposed && disposing)
