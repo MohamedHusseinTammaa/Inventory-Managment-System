4b33928943a27c7864d105a0886ae026458bfe50
Mohamed Hussein <muhammed.tammaa@gmail.com>
2025-04-03 05:48:38 +0200

    create views for order

---
+++ Services/CustomerServices.cs
@@ -0,0 +1,22 @@
+﻿using Inventory_Managment_System.Interfaces;
+using Inventory_Managment_System.Models.Classes;
+using Inventory_Managment_System.UnitOfWork;
+using System.Collections;
+
+namespace Inventory_Managment_System.Services
+{
+    public class CustomerServices : ICustomer
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CustomerServices(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllCustomers()
+        {
+            return await _unitOfWork.Repository<Customer>().getAllAsync();
+        } 
+    }
+}
